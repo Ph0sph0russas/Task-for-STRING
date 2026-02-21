521b75a29cfc3b94e1a9cb7b3d76337b80b8239b
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-02-22 00:27:54 +0800

    Первая версия программы

---
+++ Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = Console.ReadLine();
+            string secondWord = Console.ReadLine();
+            for (int i = 0; i<firstWord.Length;i++)
+            {
+                for (int j = i+1;j<firstWord.Length;j++)
+                {
+                    if (char.ToLower(firstWord[i]) == char.ToLower(firstWord[j]))
+                    {
+                        firstWord=firstWord.Remove(j, 1);
+                        j--;
+                    }
+                }
+            }
+            bool detected = false;
+            for (int i = 0;i<firstWord.Length;i++)
+            {
+                for (int j =0;j<secondWord.Length;j++)
+                {
+                    if (char.ToLower(firstWord[i]) == char.ToLower(secondWord[j]))
+                    {
+                        detected = true;
+                        break;
+                    }
+                }
+                if (detected)
+                {
+                    Console.Write("да ");
+                    detected = false;
+                }
+                else 
+                { 
+                    Console.Write("нет "); 
+                }
+            }
+
+        }
+    }
+}
